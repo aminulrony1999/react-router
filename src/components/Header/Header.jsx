708640b1242868f7402d9f85e864a1b279b4e315
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-12 12:18:33 +0600

    Active Link added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Header = () => {
     return (
         <nav>
-            <Link to="/">Home</Link><Link to="/about">About</Link> <Link to="/contact">Contact</Link> <Link to="/friends">Friends</Link>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link> 
+            <Link to="/contact">Contact</Link> 
+            <ActiveLink to="/friends">Friends</ActiveLink>
         </nav>
     );
 };
