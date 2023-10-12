71f82aafd2b00b5ae6c3a57f55967d3038ad54b5
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-12 11:55:35 +0600

    back button activated using useNavigate hook

--- src/components/FriendDetail/FriendDetail.jsx
+++ src/components/FriendDetail/FriendDetail.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const FriendDetail = () => {
     const {name, phone} = useLoaderData();
+    const navigate = useNavigate();
     return (
         <div>
             <h3>Name : {name}</h3>
             <p>Phone : {phone}</p>
+            <button onClick={() => navigate(-1)}>Back previous page.</button>
         </div>
     );
 };
